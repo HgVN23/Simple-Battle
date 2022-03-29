e61cd2025bedde1e8a677a0a76ae95bd42bdad06
Hg <83497023+HgVN23@users.noreply.github.com>
2022-03-29 23:41:36 +0700

    Add files via upload

---
+++ assets/javascript/defaults.js
@@ -0,0 +1,10 @@
+function defaults() {
+	// ======================================================================
+	// Script
+	// for (let script of scriptList) {
+	// 	let scriptCreate = document.createElement('script');
+	// 	scriptCreate.src = `https://hgvn23.github.io/assets/javascript/${script}.js`;
+	// 	document.getElementById("script").appendChild(scriptCreate);
+	// };
+	// ======================================================================
+}
