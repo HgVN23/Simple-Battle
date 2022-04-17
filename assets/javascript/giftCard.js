264530a39f938ac1f0489996f7e03f0e0aaeff14
Hg <83497023+HgVN23@users.noreply.github.com>
2022-04-17 16:47:22 +0700

    Add files via upload

---
+++ assets/javascript/giftCard.js
@@ -0,0 +1,77 @@
+let giftDeck = [
+	"giftCard1",
+	"giftCard2",
+	"giftCard3",
+	"giftCard4",
+	"giftCard5",
+	"giftCard6",
+	"giftCard7",
+	"giftCard8",
+	"giftCard9"
+];
+
+let giftCard1 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol sword"></div>
+		<div class="symbol sword"></div>
+	</div>
+`;
+let giftCard2 = `
+	<div class="card gift">
+		<div class="symbol shield"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol shield"></div>
+	</div>
+`;
+let giftCard3 = `
+	<div class="card gift">
+		<div class="symbol speed"></div>
+		<div class="symbol speed"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
+let giftCard4 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
+let giftCard5 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let giftCard6 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol speed"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let giftCard7 = `
+	<div class="card gift">
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let giftCard8 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let giftCard9 = `
+	<div class="card gift">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+		<div class="symbol anything"></div>
+	</div>
+`;
---
+++ assets/javascript/handCards.js
@@ -0,0 +1,149 @@
+let handDesk = [
+	"handCard1",
+	"handCard2",
+	"handCard3",
+	"handCard4",
+	"handCard5",
+	"handCard6",
+	"handCard7",
+	"handCard8",
+	"handCard9",
+	"handCard10",
+	"handCard11",
+	"handCard12",
+	"handCard13",
+	"handCard14",
+	"handCard15",
+	"handCard16",
+	"handCard17",
+	"handCard18",
+	"handCard19",
+	"handCard20",
+	"handCard21"
+];
+
+let handCard1 = `
+	<div class="card lvl1">
+		<div class="symbol sword"></div>
+	</div>
+`;
+let handCard2 = `
+	<div class="card lvl1">
+		<div class="symbol sword"></div>
+	</div>
+`;
+let handCard3 = `
+	<div class="card lvl1">
+		<div class="symbol sword"></div>
+	</div>
+`;
+let handCard4 = `
+	<div class="card lvl1">
+		<div class="symbol sword"></div>
+	</div>
+`;
+let handCard5 = `
+	<div class="card lvl1">
+		<div class="symbol shield"></div>
+	</div>
+`;
+let handCard6 = `
+	<div class="card lvl1">
+		<div class="symbol shield"></div>
+	</div>
+`;
+let handCard7 = `
+	<div class="card lvl1">
+		<div class="symbol shield"></div>
+	</div>
+`;
+let handCard8 = `
+	<div class="card lvl1">
+		<div class="symbol shield"></div>
+	</div>
+`;
+let handCard9 = `
+	<div class="card lvl1">
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard10 = `
+	<div class="card lvl1">
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard11 = `
+	<div class="card lvl1">
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard12 = `
+	<div class="card lvl1">
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard13 = `
+	<div class="card lvl1">
+		<div class="symbol anything"></div>
+	</div>
+`;
+let handCard14 = `
+	<div class="card lvl1">
+		<div class="symbol anything"></div>
+	</div>
+`;
+let handCard15 = `
+	<div class="card lvl1">
+		<div class="symbol anything"></div>
+	</div>
+`;
+let handCard16 = `
+	<div class="card lvl2">
+		<div class="symbol sword"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let handCard17 = `
+	<div class="card lvl2">
+		<div class="symbol shield"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let handCard18 = `
+	<div class="card lvl2">
+		<div class="symbol speed"></div>
+		<div class="symbol double"></div>
+	</div>
+`;
+let handCard19 = `
+	<div class="card lvl2">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+	</div>
+`;
+let handCard20 = `
+	<div class="card lvl2">
+		<div class="symbol sword"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard21 = `
+	<div class="card lvl2">
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard22 = `
+	<div class="card lvl3">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
+let handCard23 = `
+	<div class="card lvl3">
+		<div class="symbol sword"></div>
+		<div class="symbol shield"></div>
+		<div class="symbol speed"></div>
+	</div>
+`;
